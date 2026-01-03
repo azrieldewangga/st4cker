6df9fa6662fc32b7f866bad7437ef2342c97dcdc
azrieldewangga <rilazhd@gmail.com>
2026-01-04 02:11:12 +0700

    splashscreen fix

--- scripts/ensure-electron-config.js
+++ scripts/ensure-electron-config.js
@@ -17,3 +17,16 @@ const content = JSON.stringify({ type: 'module' }, null, 4);
 
 fs.writeFileSync(pkgPath, content);
 console.log('Successfully created dist-electron/package.json');
+
+// Copy splash.html to dist-electron
+console.log('Copying splash.html to dist-electron...');
+const splashSrc = path.resolve('electron/splash.html');
+const splashDest = path.join(distPath, 'splash.html');
+
+if (fs.existsSync(splashSrc)) {
+    fs.copyFileSync(splashSrc, splashDest);
+    console.log('Successfully copied splash.html');
+} else {
+    console.error('ERROR: electron/splash.html not found!');
+    process.exit(1);
+}
